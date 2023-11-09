3ac0ebc1d400cb53e300e1b728c006fb96e127d7
tusmun3105 <150321049+tusmun3105@users.noreply.github.com>
2023-11-09 10:06:03 +0400

    Add files via upload

---
+++ webHackt/app.py
@@ -0,0 +1,12 @@
+# save this as app.py
+from flask import Flask, jsonify, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
